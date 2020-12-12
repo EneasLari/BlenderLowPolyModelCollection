befd5498a1de1d8ec8668af35c6c523735246bb2
EneasLari <cs02367@cc.uoi.gr>
2020-12-12 02:20:21 +0200

    image loading

--- loadpreviewImages.js
+++ loadpreviewImages.js
@@ -29,13 +29,23 @@ function loadmore() {
             console.log(error);
         })
         .then(function () {
+            var copypreview=previewsection.cloneNode(true);
             for (i = 0; i < array.length; i++) {
                 var image = array[i]
                 var imagecolcopy = imagecol.cloneNode(true);
                 var card = imagecolcopy.querySelector("div[class=card]");
+                //console.log(i%3)
+                
+                if(i%3==0){
+                    //console.log(i)
+                    copypreview=previewsection.cloneNode(true);
+                    copypreview.innerHTML=null
+                   
+                }
                 card.innerHTML = null
                 card.appendChild(image)
-                previewsection.appendChild(imagecolcopy.children[0])
+                copypreview.appendChild(imagecolcopy.children[0])
+                previewsection.parentElement.insertAdjacentElement('beforeend', copypreview);    
             }
             loadModal();
         });
